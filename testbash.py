bd5287c67ea652b8b46fa55dbd5f75fdc52a3698
cgreydak <cgreydak@cisco.com>
2022-05-14 22:36:28 -0400

    create new repository

---
+++ testbash.py
@@ -0,0 +1,5 @@
+# This is a sample Python script.
+import subprocess
+print ("start")
+subprocess.call("sh /home/cgreydak/testbash.sh", shell=True)
+print ("end")
---
+++ testbash.sh
@@ -0,0 +1,6 @@
+echo this is a test
+sleep 5 
+ls -laR ~ | grep Loop
+sleep 5
+df -h
+echo this is space utilization
