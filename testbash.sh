2624875d54fa9c260e8d57f54632b320398210d5
cgreydak <cgreydak@cisco.com>
2022-05-16 22:34:57 -0400

    some cleanup

--- testbash.sh
+++ testbash.sh
@@ -2,15 +2,17 @@ echo this is a test
 echo wait 5 seconds
 sleep 5
 cd ~
-ls -laR ~ | grep Loop
+ls -laR  | grep Loop
 echo wait 5 seconds
 sleep 5
 echo
 echo
-See Space Utilizaton
+echo See Space Utilizaton
+echo
+echo
 df -h
 echo
-
+echo
 echo This is space utilization
 echo
 
